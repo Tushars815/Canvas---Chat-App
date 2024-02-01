308f573bac26a0222f597512707d31eddbccbc99
Tushars815 <96690270+Tushars815@users.noreply.github.com>
2024-02-01 23:17:40 +0530

    backend deployed updates

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "https://amigo-front.vercel.app",
+    origin: "*",
     credentials: true,
   },
 });
