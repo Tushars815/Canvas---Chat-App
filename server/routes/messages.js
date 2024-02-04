9a2cab7f7d2f22d6542fde484d8a1d8b31420f26
Tushars815 <96690270+Tushars815@users.noreply.github.com>
2024-02-05 00:28:56 +0530

    Time bug update

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -2,42 +2,48 @@ const Messages = require("../models/messageModel");
 const router = require("express").Router();
 
 router.post("/addmsg/", async (req, res, next) => {
-    try {
-      const { from, to, message } = req.body;
-      const data = await Messages.create({
-        message: { text: message },
-        users: [from, to],
-        sender: from,
-      });
-  
-      if (data) return res.json({ msg: "Message added successfully." });
-      else return res.json({ msg: "Failed to add message to the database" });
-    } catch (ex) {
-      next(ex);
-    }
-  });
+  try {
+    const { from, to, message } = req.body;
+    const data = await Messages.create({
+      message: { text: message },
+      users: [from, to],
+      sender: from,
+    });
+
+    if (data) return res.json({ msg: "Message added successfully." });
+    else return res.json({ msg: "Failed to add message to the database" });
+  } catch (ex) {
+    next(ex);
+  }
+});
 router.post("/getmsg/", async (req, res, next) => {
-    try {
-      const { from, to } = req.body;
-  
-      const messages = await Messages.find({
-        users: {
-          $all: [from, to],
-        },
-      }).sort({ updatedAt: 1 });
-      const options = { hour: '2-digit', minute: '2-digit' };
-      const projectedMessages = messages.map((msg) => {
-        return {
-          fromSelf: msg.sender.toString() === from,
-          message: msg.message.text,
-          realTime:msg.createdAt.toLocaleTimeString([],options),
-          realDate: msg.createdAt.toLocaleDateString()
-        };
-      });
-      res.json(projectedMessages);
-    } catch (ex) {
-      next(ex);
-    }
-  });
+  try {
+    const { from, to } = req.body;
+
+    const messages = await Messages.find({
+      users: {
+        $all: [from, to],
+      },
+    }).sort({ updatedAt: 1 });
+    const options = {
+      hour: "2-digit",
+      minute: "2-digit",
+      timeZone: "Asia/Kolkata",
+    };
+    const projectedMessages = messages.map((msg) => {
+      return {
+        fromSelf: msg.sender.toString() === from,
+        message: msg.message.text,
+        realTime: msg.createdAt.toLocaleTimeString([], options),
+        realDate: msg.createdAt.toLocaleDateString([], {
+          timeZone: "Asia/Kolkata",
+        }),
+      };
+    });
+    res.json(projectedMessages);
+  } catch (ex) {
+    next(ex);
+  }
+});
 
 module.exports = router;
